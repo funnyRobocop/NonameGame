484fb0f18036c5e4f793263e4ee9aa57387d42c7
Alikin_Alexey <funnyrobocop@gmail.com>
2026-08-19 16:47:03 +0300

    timer

--- Assets/Scripts/UI/NetworkLevelFinishView.cs
+++ Assets/Scripts/UI/NetworkLevelFinishView.cs
@@ -1,13 +1,26 @@
+using System.Linq;
+using Fusion;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NetworkLevelFinishView : MonoBehaviour
 {
     [Header("Элементы UI")]
     [SerializeField] private GameObject qualifiedPanel;
     [SerializeField] private TextMeshProUGUI placeText; 
+    [SerializeField] private Button leaveButton;
 
     private NetworkPlayerController localPlayer;
+
+    private void Start()
+    {
+        if (leaveButton != null)
+        {
+            leaveButton.onClick.AddListener(LeaveSessionAndReturnToMenu);
+        }
+    }
 
     private void Update()
     {
@@ -33,11 +46,25 @@ public class NetworkLevelFinishView : MonoBehaviour
 
     private NetworkPlayerController FindLocalPlayer()
     {
-        var allPlayers = FindObjectsByType<NetworkPlayerController>(FindObjectsSortMode.None);
+        var allPlayers = FindObjectsByType<NetworkPlayerController>();
         foreach (var player in allPlayers)
         {
             if (player.HasInputAuthority) return player;
         }
         return null;
+    }
+    
+    private async void LeaveSessionAndReturnToMenu()
+    {
+        NetworkRunner runner = FindObjectsByType<NetworkRunner>().FirstOrDefault();
+
+        if (runner != null)
+        {
+            Debug.Log("[Сеть] Выходим из игры. Закрываем сетевую сессию Photon...");
+            
+            await runner.Shutdown();
+        }
+
+        SceneManager.LoadScene(0);
     }
 }
