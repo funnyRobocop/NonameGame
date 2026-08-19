b076b63e6dbfe9518969fc7f22987ec7d852a333
Alikin_Alexey <funnyrobocop@gmail.com>
2026-08-20 02:55:59 +0300

    upd

--- Assets/Scripts/Network/NetworkCheckpoint.cs
+++ Assets/Scripts/Network/NetworkCheckpoint.cs
@@ -3,11 +3,13 @@ using Fusion;
 
 public class NetworkCheckpoint : NetworkBehaviour
 {
+    private bool _isActivated;
+
     private void OnTriggerEnter(Collider other)
     {
         if (!Runner.IsServer) return;
 
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !_isActivated)
         {
             var playerController = other.GetComponent<NetworkPlayerController>();
             if (playerController != null)
@@ -16,6 +18,7 @@ public class NetworkCheckpoint : NetworkBehaviour
                 // Используем позицию самого чекпоинта (или пустой объект-точку чуть выше него)
                 Vector3 savePosition = transform.position + Vector3.up * 1f;
                 playerController.UpdateCheckpoint(savePosition);
+                _isActivated = true;
                 
                 Debug.Log($"[Сервер] Игрок {Object.InputAuthority} сохранил чекпоинт на позиции {savePosition}");
             }
